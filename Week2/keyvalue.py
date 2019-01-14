10864a238874b0880a7f85d4ce7d3449767c2c4b
Katuhis <katuhis@yandex.ru>
2019-01-14 15:53:17 +0300

    regular

--- Week2/keyvalue.py
+++ Week2/keyvalue.py
@@ -1,23 +1,17 @@
-##import os
-##import tempfile
-
-##storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-##with open(storage_path, 'w') as f:
+import os
+import tempfile
 
 import argparse
 
-parser = argparse.ArgumentParser(description="calculate X to the power of Y")
-group = parser.add_mutually_exclusive_group()
-parser.add_argument("x", type=int, help="the base")
-parser.add_argument("y", type=int, help="the exponent")
-group.add_argument("-v", "--verbose", action="store_true")
-group.add_argument("-q", "--quiet", action="store_true")
+parser = argparse.ArgumentParser()
+parser.add_argument("--key", help="the key")
+parser.add_argument("--value", help="the value")
 args = parser.parse_args()
-answer = args.x ** args.y
 
-if args.quiet:
-    print(answer)
-elif args.verbose:
-    print("{} to the power {} equals {}".format(args.x, args.y, answer))
+if args.value is None:
+    print(1)
 else:
-    print("{}^{} == {}".format(args.x, args.y, answer))
+    print(2)
+
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+with open(storage_path, 'w') as f:
