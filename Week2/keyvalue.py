c6aac4fa97b814f82b9298e331ce3376895183c0
Katuhis <katuhis@yandex.ru>
2019-01-14 16:34:46 +0300

    regular

--- Week2/keyvalue.py
+++ Week2/keyvalue.py
@@ -3,15 +3,29 @@ import tempfile
 
 import argparse
 
+import json
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--key", help="the key")
 parser.add_argument("--value", help="the value")
 args = parser.parse_args()
 
-if args.value is None:
-    print(1)
-else:
-    print(2)
+keyvalue = dict()
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-with open(storage_path, 'w') as f:
+if os.stat(storage_path).st_size > 0:
+    with open(storage_path, 'r') as f:
+        keyvalue = json.load(f)
+
+if args.value is None:
+    print(keyvalue[args.key])
+else:
+    if args.key in keyvalue:
+        keyvalue[args.key] = keyvalue[args.key] + ', ' + args.value
+    else:
+        keyvalue[args.key] = args.value
+
+    with open(storage_path, 'w') as f:
+        json.dump(keyvalue, f)
+
+
