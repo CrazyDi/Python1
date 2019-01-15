211a5dcd2c76164b8d91b8f467050f2d8a31865d
Katuhis <katuhis@yandex.ru>
2019-01-15 17:30:30 +0300

    regular

--- Week2/storage.py
+++ Week2/storage.py
@@ -5,20 +5,27 @@ import argparse
 
 import json
 
+## разбираемся с аргументами
 parser = argparse.ArgumentParser()
 parser.add_argument("--key", help="the key")
 parser.add_argument("--value", help="the value")
 args = parser.parse_args()
 
+## наше хранилищщщщщщщщщщще
 keyvalue = dict()
 
+## файл
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 if os.path.exists(storage_path):
+    ## открываем
     with open(storage_path, 'r') as f:
+        ## читаем сразу в строку
         keyvalue = json.load(f)
 
+## если значение не ввели, выводим значение по ключу
 if args.value is None:
     print(keyvalue[args.key])
+## если ввели, добавляем по ключу
 else:
     if args.key in keyvalue:
         keyvalue[args.key] = keyvalue[args.key] + ', ' + args.value
