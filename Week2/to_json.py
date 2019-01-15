31c78fc3638c8c334b3eed098bd7e449676c17f0
Katuhis <katuhis@yandex.ru>
2019-01-15 17:31:39 +0300

    regular

--- Week2/to_json.py
+++ Week2/to_json.py
@@ -2,9 +2,12 @@ import json
 import functools
 
 def to_json(func):
+    ## фигня, которая нормально именует функцию
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
+        ## наша функция
         result = func(*args, **kwargs)
+        ## возвращаем результат, преобразованный в JSON
         return json.dumps(result)
     return wrapped
 
