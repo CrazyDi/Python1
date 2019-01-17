1562ba5f989e5925512c78fbc06f94a1b72a4da9
Katuhis <katuhis@yandex.ru>
2019-01-17 17:18:45 +0300

    regular

---
+++ Week3/city.py
@@ -0,0 +1,16 @@
+import pprint
+
+class CityInfo:
+
+    def __init__(self, city):
+        self.city = city
+
+    def weather_forecast(self):
+        pass
+
+def _main():
+    city_info = CityInfo("Minsk")
+    pprint.pprint(forecast)
+
+if __name__ == "__main__":
+    _main()
