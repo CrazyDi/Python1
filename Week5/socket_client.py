b94e9c8a1fe4b6ce0a7d8e5392ce7b515ba95272
Katuhis <katuhis@yandex.ru>
2019-01-30 15:08:58 +0300

    regular

--- Week5/socket_client.py
+++ Week5/socket_client.py
@@ -6,9 +6,8 @@ if __name__ == "__main__":
     #  sock.connect(("127.0.0.1", 10001))
     #   sock = socket.create_connection(('127.0.0.1', 10001))
     # try:
-    with socket.create_connection(('127.0.0.1', 10001), 5) as sock:
+    with socket.create_connection(('127.0.0.1', 10001)) as sock:
         sock.sendall('bla'.encode("utf8"))
-        sock.sendall('False'.encode('utf8'))
     # except:
     #   print("open connection data error")
     #  sock.close()
