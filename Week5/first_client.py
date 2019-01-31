19e391c5e2417f5f4ffcfcd73b37f9e27938362e
Katuhis <katuhis@yandex.ru>
2019-01-31 14:34:20 +0300

    regular

---
+++ Week5/first_client.py
@@ -0,0 +1,12 @@
+import socket
+
+if __name__ == "__main__":
+    HOST = '127.0.0.1'
+    POST = 50007
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, POST))
+        s.sendall(b'Hello world')
+        data = s.recv(1024)
+
+    print('Received', repr(data))
