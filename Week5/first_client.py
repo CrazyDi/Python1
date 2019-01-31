f6c3c9fa3c289b841454f611292815f46d2c3cc3
Katuhis <katuhis@yandex.ru>
2019-01-31 16:40:47 +0300

    regular

--- Week5/first_client.py
+++ Week5/first_client.py
@@ -2,7 +2,7 @@ import socket
 
 if __name__ == "__main__":
     HOST = '127.0.0.1'
-    POST = 50007
+    POST = 10001
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, POST))
